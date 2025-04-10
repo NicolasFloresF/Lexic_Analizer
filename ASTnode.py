14d828a766e8450c5a815803e584119e748e9921
NicolasFloresF <nick.lasff@gmail.com>
2025-04-10 16:54:30 -0300

    [UPDATE] AST visualization added

--- ASTnode.py
+++ ASTnode.py
@@ -1,4 +1,5 @@
 import ply.yacc as yacc
+from graphviz import Digraph
 
 
 class ASTnode:
@@ -22,3 +23,30 @@ class ASTnode:
             else:
                 ret += "  " * (level + 1) + repr(child) + "\n"
         return ret
+
+    def to_graphviz(self, dot=None, parent=None, counter=None):
+        if dot is None:
+            dot = Digraph()
+            dot.attr(rankdir="TB")
+            counter = [0]
+
+        node_id = f"node{counter[0]}"
+        label = f"{self.nodetype}"
+        if self.value is not None:
+            label += f"\\n{self.value}"
+        dot.node(node_id, label)
+        counter[0] += 1
+
+        if parent is not None:
+            dot.edge(parent, node_id)
+
+        for child in self.children:
+            if isinstance(child, ASTnode):
+                child.to_graphviz(dot, node_id, counter)
+            else:
+                child_id = f"node{counter[0]}"
+                dot.node(child_id, str(child))
+                dot.edge(node_id, child_id)
+                counter[0] += 1
+
+        return dot
