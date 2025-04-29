f862a8a4deb8445dcd9a43fa866db4e06b4ff64e
NicolasFloresF <nick.lasff@gmail.com>
2025-04-29 00:07:22 -0300

    [REFACTOR] main logic and printing refactored

--- __main__.py
+++ __main__.py
@@ -6,54 +6,81 @@ import os, sys
 from tokens import *
 from grammar import *
 from semantic_analyzer import *
+from code_generator import *
 
 
 def read_file(file_name):
-    file_object = open(file_name, "r")
+    file = open(file_name, "r")
 
-    return file_object.read()
+    return file.read()
 
 
-def print_tokens(lexer):
+def print_tokens(lexer, debug):
     while True:
         tok = lexer.token()
         if not tok:
             break
-        print(tok)
+        print(tok, file=debug)
 
 
-def print_table(table):
+def print_table(table, debug):
     for name, symbol in table.symbols.items():
-        print(f"{name}: {symbol}")
+        print(f"{name}: {symbol}", file=debug)
 
 
 def main():
+    # get the CLI (stdout) and debug file name
     stdout = sys.stdout
-    sys.stdout = open(args.debug, "w")
+    debug = open(args.debug, "w")
 
+    # instantiate the lexer and run it on the input data
     lexer = lex.lex()
     lexer.input(data)
-    print("Tokens:")
-    print_tokens(lexer)
 
+    # print the tokens to the debug file
+    print("Tokens:", file=debug)
+    print_tokens(lexer, debug)
+
+    # reset line number and do the syntax parsing
     lexer.lineno = 1
-    parser = yacc.yacc(debug=True)
+    parser = yacc.yacc(debug=False)
     syntaxParsing = parser.parse(data, lexer=lexer)
-    print("AST:")
-    print(syntaxParsing)
+
+    # print the syntax tree to the debug file
+    print("AST:", file=debug)
+    print(syntaxParsing, file=debug)
+
+    # plot the syntax tree in a graphviz format
     dot = syntaxParsing.to_graphviz()
     dot.render("ast", format="png", cleanup=True)
 
-    sys.stdout = stdout
-    tables = []
-    for functions in syntaxParsing.children[1].children:
-        tables.append(build_symbol_table(functions))
-        semantic_analysis(functions, tables[-1])
+    # get the function list from syntax tree
+    function_list = syntaxParsing.children[1].children
 
-    sys.stdout = open(args.debug, "a")
-    print("Symbol Table:")
-    for table in tables:
-        print_table(table)
+    # define the symbol tables
+    local_tables: list[symbol_table] = []
+    global_table = build_global_table(function_list)
+
+    # print the global table to the debug file
+    print(f"Parent: {global_table.return_parent()}", file=debug)
+    print_table(global_table, debug=debug)
+
+    # itarate through the function list, build local tables and do semantic analysis
+    for function in function_list:
+        local_tables.append(build_local_table(function))
+        semantic_analysis(function, local_tables[-1], global_table)
+
+    # print the local tables to the debug file
+    for table in local_tables:
+        # to get global table function from a local table, do global_table.return_table()[table.return_parent()
+        print(f"Parent: {table.return_parent()}", file=debug)
+        print(f"Details: {global_table.return_table()[table.return_parent()]}", file=debug)
+        print_table(table, debug)
+
+    with open(args.file[:-4] + ".asm", "w") as sys.stdout:
+        generate_code(function_list, local_tables, global_table)
+
+    print("Compilation finished successfully!", file=stdout)
 
 
 if __name__ == "__main__":
